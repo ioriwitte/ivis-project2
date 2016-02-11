4ab5d175459c8c4f5cdf1c6f49018e05afa2abda
Zhihao Yang <yang_zhihao@live.com>
2016-02-11 16:45:27 +0100

    edit website structure

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 // ------------------------
-// Set X axis and Y axia
+// Set Prosition for the graph
 // ------------------------
 
 // set the stage for the visualization
-var margin = {top: 20, right: 80, bottom: 30, left: 50},
-    w = 700 - margin.left - margin.right,
+var margin = {top: 50, right: 50, bottom: 50, left: 50},
+    w = 800 - margin.left - margin.right,
     h = 500 - margin.top - margin.bottom,
     x = d3.scale.linear().range([0, w]),
     y = d3.scale.linear().range([h, 0]);
@@ -18,50 +18,49 @@ var categories,
 
 // where the line gets its properties, how it will be interpolated
 var line = d3.svg.line()
-   .interpolate("basis")
-   .x(function(d) { return x(d.educationLevel); })
-   .y(function(d) { return y(d.stat); });
+                 .interpolate("basis")
+                 .x(function(d) { return x(d.educationLevel); })
+                 .y(function(d) { return y(d.stat); });
 
 // add svg box where viz will go    
-var svg = d3.select("body").append("svg")
-    .attr("width", w + margin.left + margin.right)
-    .attr("height", h + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select("#display_svg").append("svg")
+            .attr("width", w + margin.left + margin.right)
+            .attr("height", h + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // define the x axis and its class, append it to svg 
 var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
-    .ticks(5)
-   svg.append("svg:g")
-    .attr("class", "x axis");
+                  .scale(x)
+                  .orient("bottom")
+                  .ticks(5)
+svg.append("svg:g")
+   .attr("class", "x axis");
 
 // define the y axis and its class, append it to svg
 var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    svg.append("svg:g")
-    .attr("class", "y axis");
+              .scale(y)
+              .orient("left")
+svg.append("svg:g")
+   .attr("class", "y axis");
 
 // ------------------------
 // Update data
 // ------------------------
 
-// force data to update when menu is changed    
-var menu = d3.select("#menu select")
-.on("change", change);    
+// force data to update when year_period_menu is changed    
+var year_period_menu = d3.select("#year_period_menu select").on("change", change);    
 
 // put data from csv into categories variable
 //run redraw function that will refresh whenever a new data series is selected 
 d3.csv("data.csv", function(csv) {
-     categories = csv;
-     redraw();
+    categories = csv;
+    redraw();
 });
 
 d3.select(window)
-    .on("keydown", function() { altKey = d3.event.altKey; })
-    .on("keyup", function() { altKey = false; });
+  .on("keydown", function() { altKey = d3.event.altKey; })
+  .on("keyup", function() { altKey = false; });
 
 var altKey;
 
@@ -71,8 +70,8 @@ function change() {
   // clearTimeout(timeout);
 
   d3.transition()
-      .duration(altKey ? 7500 : 1500)
-      .each(redraw);
+    .duration(altKey ? 7500 : 1500)
+    .each(redraw);
   }
 
 // all the meat goes in the redraw function
@@ -83,10 +82,10 @@ function redraw() {
     .key(function(d) { return d.yearPeriodCode; })
     .map(categories)
 
-    // get value from menu selection
+    // get value from year_period_menu selection
     // the option values are set in HTML and correspond
     //to the [yearPeriodCode] value we used to nest the data 
-    var series = menu.property("value");
+    var series = year_period_menu.property("value");
     
     // only retrieve data from the selected series, using the nest we just created
     var data = nested[series];
@@ -99,18 +98,18 @@ function redraw() {
     // for object constancy we will need to set "keys", one for each catetory.
     // the keyring variable contains only the names of the categories
     var keyring = d3.keys(data[0]).filter(function(key) { 
-          return (key !== "country" && key !== "countryCode" && key !== "yearPeriod" && key !== "yearPeriodCode" && key !== "educationLevel" && key !== "educationLevelCode");
-      });
+        return (key !== "country" && key !== "countryCode" && key !== "yearPeriod" && key !== "yearPeriodCode" && key !== "educationLevel" && key !== "educationLevelCode");
+    });
     
     // get the educationLevel and related statistics, map them to each categories separately 
     var transpose = keyring.map(function(name) {
-            return {
-              name: name,
-              values: data.map(function(d) {
-                return {educationLevel: d.educationLevel, stat: +d[name]};
-              })
-            };
-        });
+          return {
+            name: name,
+            values: data.map(function(d) {
+              return {educationLevel: d.educationLevel, stat: +d[name]};
+            })
+          };
+      });
 
     // set the x and y domains as the max and min
     // of the related year and statistics, respectively
@@ -137,12 +136,12 @@ function redraw() {
     // create lables for each categories
     // set their position to that of the last educationLevel and stat
     categoryEnter.append("text")
-     .attr("class", "names")
-     .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-     .attr("transform", function(d) { return "translate(" + x(d.value.educationLevel) + "," + y(d.value.stat) + ")"; })
-     .attr("x", 4)
-     .attr("dy", ".35em")
-     .text(function(d) { return d.name; });
+      .attr("class", "names")
+      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+      .attr("transform", function(d) { return "translate(" + x(d.value.educationLevel) + "," + y(d.value.stat) + ")"; })
+      .attr("x", 4)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.name; });
 
     // set variable for updating visualization
     var categoryUpdate = d3.transition(category);
@@ -154,15 +153,15 @@ function redraw() {
     
     // change position of text alongside the moving path  
     categoryUpdate.select("text")
-       .attr("transform", function(d) { return "translate(" + x(d.values[d.values.length - 1].educationLevel) + "," + y(d.values[d.values.length - 1].stat) + ")"; });
+      .attr("transform", function(d) { return "translate(" + x(d.values[d.values.length - 1].educationLevel) + "," + y(d.values[d.values.length - 1].stat) + ")"; });
   
   // update the axes, though only the y axis will change    
-      d3.transition(svg).select(".y.axis")
-          .call(yAxis);   
+    d3.transition(svg).select(".y.axis")
+      .call(yAxis);   
           
-      d3.transition(svg).select(".x.axis")
-            .attr("transform", "translate(0," + h + ")")
-          .call(xAxis);
+    d3.transition(svg).select(".x.axis")
+      .attr("transform", "translate(0," + h + ")")
+      .call(xAxis);
           
 // that concludes redraw()
 }
